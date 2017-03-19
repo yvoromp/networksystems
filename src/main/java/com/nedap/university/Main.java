a788af211698d667388ed0b79b0e6213d855ea5c
Hendrik van der Linde <hendrik.vanderlinde@nedap.com>
2017-03-19 14:21:33 +0100

    - shutdown hook - proper shutdown sequence

--- src/main/java/com/nedap/university/Main.java
+++ src/main/java/com/nedap/university/Main.java
@@ -2,7 +2,44 @@ package com.nedap.university;
 
 public class Main {
 
+    private static boolean keepAlive = true;
+    private static boolean running = false;
+
+    private Main() {}
+
     public static void main(String[] args) {
+        running = true;
         System.out.println("Hello, Nedap University!");
+
+        initShutdownHook();
+
+        while (keepAlive) {
+            try {
+                // do useful stuff
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        System.out.println("Stopped");
+        running = false;
+    }
+
+    private static void initShutdownHook() {
+        final Thread shutdownThread = new Thread() {
+            @Override
+            public void run() {
+                keepAlive = false;
+                while (running) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        };
+        Runtime.getRuntime().addShutdownHook(shutdownThread);
     }
 }
